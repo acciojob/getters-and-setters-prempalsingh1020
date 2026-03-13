f3310d12baaa1b9767d235e27a4a793fbe07b7ce
prempalsingh1020 <prempalsingh030499@gmail.com>
2026-03-13 12:06:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-//complete this code
 class Person {
-	private _name: string;
+  private _name: string;
   private _age: number;
 
   constructor(name: string, age: number) {
@@ -8,168 +7,35 @@ class Person {
     this._age = age;
   }
 
-  // Getter for name
   get name(): string {
     return this._name;
   }
 
-  // Setter for name
   set name(name: string) {
     this._name = name;
   }
 
-  // Getter for age
   get age(): number {
     return this._age;
   }
 
-  // Setter for age
   set age(age: number) {
     this._age = age;
   }
 }
 
 class Student extends Person {
-	 study(): void {
+  study(): void {
     console.log(`${this.name} is studying`);
   }
 }
 
 class Teacher extends Person {
-	teach(): void {
+  teach(): void {
     console.log(`${this.name} is teaching`);
   }
 }
 
-// Do not change the code below this line
 window.Person = Person;
 window.Student = Student;
 window.Teacher = Teacher;
-
-
-
-
-
-
-
-
-
-
-
-class Rectangle {
-  constructor(width, height) {
-    this._width = width;
-    this._height = height;
-  }
-
-  // Getter for width
-  get width() {
-    return this._width;
-  }
-
-  // Getter for height
-  get height() {
-    return this._height;
-  }
-
-  // Method to calculate area
-  getArea() {
-    return this._width * this._height;
-  }
-}
-
-class Square extends Rectangle {
-  constructor(side) {
-    super(side, side); // width and height same
-  }
-
-  // Method to calculate perimeter
-  getPerimeter() {
-    return this.width * 4;
-  }
-}
-
-// Do not change the code below this line
-window.Rectangle = Rectangle;
-window.Square = Square;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
