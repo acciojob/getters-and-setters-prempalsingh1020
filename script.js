0a6c64ccf278f6f8718336436ab98d8e7341f31d
prempalsingh1020 <prempalsingh030499@gmail.com>
2026-03-13 12:01:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -56,7 +56,42 @@ window.Teacher = Teacher;
 
 
 
+class Rectangle {
+  constructor(width, height) {
+    this._width = width;
+    this._height = height;
+  }
 
+  // Getter for width
+  get width() {
+    return this._width;
+  }
+
+  // Getter for height
+  get height() {
+    return this._height;
+  }
+
+  // Method to calculate area
+  getArea() {
+    return this._width * this._height;
+  }
+}
+
+class Square extends Rectangle {
+  constructor(side) {
+    super(side, side); // width and height same
+  }
+
+  // Method to calculate perimeter
+  getPerimeter() {
+    return this.width * 4;
+  }
+}
+
+// Do not change the code below this line
+window.Rectangle = Rectangle;
+window.Square = Square;
 
 
 
